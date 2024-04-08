7be99f13d409e6a492b26bc50c38e031c8263b27
Charles Bergeron <chuck.bergeron@gmail.com>
2024-04-08 14:17:35 -0700

    initial import

---
+++ README.md
@@ -0,0 +1,6 @@
+# pt-v5-prize-claimer-gh-action-bot
+
+This is two simple scripts (`index.ts` and `.github/cron.testnet.yml`) to run the PoolTogether V5 Prize Claimer bot using GitHub Actions.
+
+You can simply fork this repo, set up your environment variables properly in your GitHub Settings, and run the bot periodically (using the cron directive found in `.github/cron.testnet.yml`)
+
---
+++ index.ts
@@ -0,0 +1,41 @@
+import nodeFetch from 'node-fetch';
+import { BaseProvider } from '@ethersproject/providers';
+import { downloadContractsBlob, ContractsBlob } from '@generationsoftware/pt-v5-utils-js';
+import {
+  getProvider,
+  loadPrizeClaimerEnvVars,
+  instantiateRelayerAccount,
+  runPrizeClaimer,
+  PrizeClaimerEnvVars,
+  PrizeClaimerConfig,
+  RelayerAccount,
+} from '@generationsoftware/pt-v5-autotasks-library';
+
+
+const main = async () =>{
+  const envVars: PrizeClaimerEnvVars = loadPrizeClaimerEnvVars();
+  const provider: BaseProvider = getProvider(envVars);
+
+  const relayerAccount: RelayerAccount = await instantiateRelayerAccount(
+    provider,
+    envVars.CUSTOM_RELAYER_PRIVATE_KEY,
+  );
+
+  const config: PrizeClaimerConfig = {
+    ...relayerAccount,
+    provider,
+    chainId: envVars.CHAIN_ID,
+    rewardRecipient: envVars.REWARD_RECIPIENT,
+    minProfitThresholdUsd: Number(envVars.MIN_PROFIT_THRESHOLD_USD),
+    covalentApiKey: envVars.COVALENT_API_KEY,
+  };
+
+  try {
+    const contracts: ContractsBlob = await downloadContractsBlob(config.chainId, nodeFetch);
+    await runPrizeClaimer(contracts, config);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+main() 
